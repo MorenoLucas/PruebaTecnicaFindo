2ef17363060a58891e7efbdffb98474c362eafa1
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-07-14 12:11:42 +0200

    hago scroll infinito con libreria

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
+import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  showButton = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @HostListener('window:scroll')
+  onScroll(): void {
+    const yOffset = window.pageYOffset;
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    this.showButton = (yOffset || scrollTop) > 500;
   }
-
+  onScrollTop(): void {
+    document.documentElement.scrollTop = 0;
+  }
 }
